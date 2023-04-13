4ab0df7dea176f84b39c0873a64949003eb44f63
MuratDura <105969841+MuratDura@users.noreply.github.com>
2023-04-13 22:49:54 +0300

    Add files via upload

---
+++ Garbage.java
@@ -0,0 +1,9 @@
+public class Garbage {
+    private final String value;
+    public Garbage(String value) {
+        this.value = value;
+    }
+    public String toString() {
+        return value;
+    }
+}
