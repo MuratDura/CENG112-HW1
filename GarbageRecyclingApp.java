0b91e91dc6564841d02919df5fef8aabebcdd349
MURAT <muratdura03@gmail.com>
2023-04-14 20:45:15 +0300

    FileIO

--- GarbageRecyclingApp.java
+++ GarbageRecyclingApp.java
@@ -1,8 +1,14 @@
 public class GarbageRecyclingApp {
 
     public static void main(String[] args) {
-        
-    
+        FileIO file = new FileIO();
+        IBag<Garbage> trashcan =  file.readTrashCan();
+        trashcan.displayItems();
+
+
+
+
+
     }
 
 }
