c1f2ce4439f6c2228897c3237c3c2e35bc17f237
MURAT <muratdura03@gmail.com>
2023-04-14 21:07:26 +0300

    Murat

--- GarbageRecyclingApp.java
+++ GarbageRecyclingApp.java
@@ -3,8 +3,13 @@ public class GarbageRecyclingApp {
     public static void main(String[] args) {
         FileIO file = new FileIO();
         IBag<Garbage> trashcan =  file.readTrashCan();
-        trashcan.displayItems();
 
+        MetalRecycleBin metalBin = new MetalRecycleBin();
+        OrganicRecycleBin organicBin = new OrganicRecycleBin();
+        PaperRecycleBin paperBin = new PaperRecycleBin();
+        PlasticRecycleBin plasticBin = new PlasticRecycleBin();
+        FabricRecycleBin fabricBin = new FabricRecycleBin();
+        GlassRecycleBin glassBin = new GlassRecycleBin();
 
 
 
