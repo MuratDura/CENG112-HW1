abc9f43e0afb8f925cf18b42da3475b4e987b735
MURAT <muratdura03@gmail.com>
2023-04-15 14:41:06 +0300

    Seperate

--- GarbageRecyclingApp.java
+++ GarbageRecyclingApp.java
@@ -1,7 +1,7 @@
 public class GarbageRecyclingApp {
 
     public static void main(String[] args) {
-
+        TrashCan updatedTrashcan = new TrashCan();
         FileIO file = new FileIO();
         TrashCan trashcan =  (TrashCan) file.readTrashCan();
 
@@ -11,8 +11,37 @@ public class GarbageRecyclingApp {
         PlasticRecycleBin plasticBin = new PlasticRecycleBin();
         FabricRecycleBin fabricBin = new FabricRecycleBin();
         GlassRecycleBin glassBin = new GlassRecycleBin();
-        trashcan.displayItems();
+
+
+        while(!trashcan.isEmpty()){
+            Garbage garbage = trashcan.remove();
+            String garb = garbage.toString();
+            String[] garbagelist = garb.split(",");
+            String type = garbagelist[1].trim();
+            if (garbage.equals("plastic")) {
+                if (plasticBin.transferTo(plasticBin, garbage)) {
+                    continue;
+                } else updatedTrashcan.transferTo(updatedTrashcan, garbage);
+            }
+            if (garbage.equals("metal")) {
+                if (metalBin.add(garbage)) {continue;}
+                else updatedTrashcan.transferTo(updatedTrashcan,garbage);}
+            if (garbage.equals("glass")) {
+                if (glassBin.transferTo(glassBin, garbage)) {continue;}
+                else updatedTrashcan.transferTo(updatedTrashcan,garbage);}
+            if (garbage.equals("paper")) {
+                if (paperBin.transferTo(paperBin, garbage)) {continue;}
+                else updatedTrashcan.transferTo(updatedTrashcan,garbage);}
+            if (garbage.equals("organic")) {
+                if (organicBin.transferTo(organicBin, garbage)) {continue;}
+                else updatedTrashcan.transferTo(updatedTrashcan,garbage);}
+            if (garbage.equals("fabric")) {
+                if (fabricBin.transferTo(fabricBin, garbage)) {continue;}
+                else updatedTrashcan.transferTo(updatedTrashcan,garbage);}
+            }
+        organicBin.displayItems();
+            }
 
     }
 
-}
+
