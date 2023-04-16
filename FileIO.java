e00d2378e5d726f2a64a94ff36351ee978487af4
MuratDura <105969841+MuratDura@users.noreply.github.com>
2023-04-16 18:43:04 +0300

    Update FileIO.java

--- FileIO.java
+++ FileIO.java
@@ -1,6 +1,50 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWritimport java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileIO {
+
+    // Reads Garbage objects from a text file and returns them as a TrashCan.
+    public IBag<Garbage> readTrashCan(){
+        @SuppressWarnings("unchecked")
+        IBag<Garbage> trashCan = new TrashCan();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("garbage.txt"));
+            String line;
+            while((line = reader.readLine()) != null) {
+                String[] garbage = line.split(",");
+                int amount = Integer.parseInt(garbage[2].trim());
+                for (int i = 0; i < amount; i++) {
+                    trashCan.add(new Garbage(garbage[0].trim(), garbage[1].trim()));
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return trashCan;
+    }
+
+    // Updates the contents of the text file with the Garbage objects in the TrashCan.
+    public boolean updateTrashCan(IBag<Garbage> trashCan) {
+        boolean result = true;
+        try {
+            FileWriter trashCanWriter = new FileWriter("updated-garbage.txt");
+            while (!trashCan.isEmpty()) {
+                String garbage = trashCan.remove().toString();
+                trashCanWriter.write(garbage + "\n");
+            }
+            trashCanWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
+
 import java.io.IOException;
 
 public class FileIO {
